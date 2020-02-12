f4bf9f3b617bed8135b36a137101563c724a0e61
Dilraj Singh <dilrajsingh.7@gmail.com>
2020-02-11 23:30:48 -0600

    Infinite scrolling (load more stories on scroll)

--- src/FrontPage.tsx
+++ src/FrontPage.tsx
@@ -5,6 +5,9 @@ const LOAD_INCREMENT = 30;
 
 const FrontPage = ({ url = 'https://hacker-news.firebaseio.com/v0/topstories.json' }) => {
   const [stories, setStories] = useState<number[]>([]);
+  const [storiesToShow, setStoriesToShow] = useState<number>(LOAD_INCREMENT);
+
+  const [doneLoading, setDone] = useState(false); // TODO add reason & check before showing more
 
   useEffect(() => {
     async function getStories() {
@@ -17,9 +20,27 @@ const FrontPage = ({ url = 'https://hacker-news.firebaseio.com/v0/topstories.jso
     getStories();
   }, [url]);
 
+  useEffect(() => {
+    const onScroll = () => {
+      const atBottom =
+        (window.innerHeight + document.documentElement.scrollTop) === document.documentElement.offsetHeight;
+
+      if (atBottom) {
+        setStoriesToShow(e => {
+          if (e > 0 && e === stories.length) setDone(true); // TODO
+          return Math.min(e + LOAD_INCREMENT, stories.length);
+        })
+      }
+    }
+
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [stories]);
+
   return (
     <div className="p-2 mx-auto" style={{ maxWidth: '600px' }}>
       {stories.slice(0, storiesToShow).map((id, index) => <Story key={id} id={id} rank={index + 1} />)}
+      {doneLoading && <div>All Done, time to go outside.</div>}
     </div>
   );
 }
