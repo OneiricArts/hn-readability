6bed4094266ce5d2dd276195e04ede52a1778621
Dilraj Singh <dilrajsingh.7@gmail.com>
2020-02-21 01:58:51 -0600

    Simple / and /item?=id routes
    Need HashRouter to support refreshing on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import './skeleton.css';
 import FrontPage from './FrontPage';
 
 const App = () => {
   return (
-    <FrontPage />
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <FrontPage />
+          </Route>
+          <Route path="/item" render={({ location }) => {
+            const searchParams = new URLSearchParams(location.search);
+            const id = searchParams.get('id');
+            return <div>item #{id} </div>
+          }}>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
