8447cbcf097336bc060bec2145aed974a786d9fc
Dilraj Singh <dilrajsingh.7@gmail.com>
2020-03-19 02:02:38 -0500

    Add front page Cypress acceptance test and CI step

---
+++ cypress/integration/frontPage.js
@@ -0,0 +1,31 @@
+/* global cy */
+
+describe('Front Page', () => {
+  it('Contains clickable stories that are sent to ', function (done) {
+    cy.visit('/');
+
+    cy.get('a')
+      .eq(1)
+      .invoke('attr', 'href')
+      .then(href => {
+        const item = parseInt(href.split('=')[1], 10);
+
+        // cy.visit(href);
+        cy.get('a')
+          .eq(1)
+          .invoke('removeAttr', 'target')
+          .click()
+          .should(() => {
+            expect(localStorage.getItem('HNR_VIEWED_STORIES_CACHE')).to.eq(JSON.stringify([item]));
+          });
+
+        cy.url().should('include', `/item?id=${item}`);
+        cy.go('back')
+          .location()
+          .should((loc) => {
+            expect(loc.pathname).to.eq('/')
+          })
+          .then(() => done());
+      });
+  });
+});
