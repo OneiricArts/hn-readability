c22dd14961775ff19d6ad2fd5f344190cb16f6dd
Dilraj Singh <dilrajsingh.7@gmail.com>
2020-04-23 23:56:41 -0500

    Move share component to separate file

--- src/item/Item.tsx
+++ src/item/Item.tsx
@@ -7,38 +7,17 @@ import { Link } from 'react-router-dom';
 import Parent from './Parent';
 import { HNItem } from '../HNApiTypes';
 import { topOfElIsVisible } from './helpers';
+import { Share } from './Share';
 
 const hNItemLink = (id: number) => `https://news.ycombinator.com/item?id=${id}`;
 
-const buttonBarClasses = "d-inline-flex align-items-center h-border-right py-2 px-2";
+export const buttonBarClasses = "d-inline-flex align-items-center h-border-right py-2 px-2";
 
 const LinkToHN = ({ id }: { id: number }) => (
   <a className={`${buttonBarClasses} hnr-inherit-color`} role="button" href={hNItemLink(id)}>
     <Icon name="link" size={1.5} />
   </a>
 );
-
-const Share = ({ title, url }: { title?: string, url: string }) => {
-  //@ts-ignore
-  if (!navigator?.share) return null;
-
-  const shareTo = async () => {
-    try {
-      //@ts-ignore
-      await navigator.share({ title, url });
-      console.log('shared!')
-    } catch (e) {
-      if (e.name === 'AbortError') console.log('Share aborted')
-      else console.log(e);
-    }
-  };
-
-  return (
-    <div className={`${buttonBarClasses} clickable`} onClick={shareTo}>
-      <Icon name="share" size={1.5} />
-    </div>
-  );
-};
 
 const LinkUrlCard = ({ url }: { url: string }) => (
   <a href={url} className="p-1 mb-2 link-card d-flex align-items-center">
